c4b6ef8eec289b9b61fd81a22802870ce099c16b
pthouvenin <pierreantoine.thouvenin@gmail.com>
2019-07-17 14:46:35 +0100

    Fix extraction real data for DR

--- real_data_dr/extract_real_data_dr.m
+++ real_data_dr/extract_real_data_dr.m
@@ -1,17 +1,20 @@
 %% real data generation
-addpath ../Dimensionality-reduced-hyper-SARA-sdwt/lib/utils/
-addpath ../Dimensionality-reduced-hyper-SARA-sdwt/fouRed/
+addpath ../lib/utils/
+addpath ../fouRed/
+addpath ../../real_data_dr
 
 %visibility_file_name = '/home/h019/aa16/S-band/CYG-S.mat';
 visibility_file_name = 'CYG_data_raw_ind=';
 param_real_data.image_size_Nx = 2560;
 param_real_data.image_size_Ny = 1536;
 nSpw = 2;
-nChannels = 2*nSpw*nSpw; % total number of channels for the real dataset 
-%                          considered
+nChannels = 2*nSpw; % total number of channels for the real dataset 
+%                     considered
+% 2*nSpw*nSpw
 klargestpercent = 20;
 extract_raw_data = true;
 %param_real_data.pixel_size = 0.3; % in arcsec
+FT2 = @(x) fftshift(fft2(ifftshift(x)));
 
 %% config parameters
 Nx = param_real_data.image_size_Nx;
@@ -94,92 +97,85 @@ if extract_raw_data
 %         end
 %     end
     % or (probably the best option so far) 'y', 'uw', 'vw', 'nWw', 'f', 'time_', 'pos'
-    k = 1;
-    y = cell(2*nSpw*nSpw, 1);
+    y = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        y{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            y{k} = cell2mat(file.y(1,l));
-            k = k+1;
+            y{l}{n} = cell2mat(file.y(1,l));
         end
     end
     save('CYG_y.mat', 'y', '-v7.3');
     clear y;
     
-    k = 1;
-    nWw = cell(2*nSpw*nSpw, 1);
+    nWw = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        nWw{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            nWw{k} = cell2mat(file.nWw(1,l));
-            k = k+1;
+            nWw{l}{n} = cell2mat(file.nWw(1,l));
         end
     end
     save('CYG_nWw.mat', 'nWw', '-v7.3');
     clear y;
     
-    k = 1;
-    uw = cell(2*nSpw*nSpw, 1);
+    uw = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        uw{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            uw{k} = cell2mat(file.uw(1,l));
-            k = k+1;
+            uw{l}{n} = cell2mat(file.uw(1,l));
         end
     end
     save('CYG_uw.mat', 'uw', '-v7.3');
     clear uw
     
-    k = 1;
-    vw = cell(2*nSpw*nSpw, 1);
+    vw = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        vw{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            vw{k} = cell2mat(file.vw(1,l));
-            k = k+1;
+            vw{l}{l} = cell2mat(file.vw(1,l));
         end
     end
     save('CYG_vw.mat', 'vw', '-v7.3');
     clear vw
     
-    k = 1;
-    f = cell(2*nSpw*nSpw, 1);
+    f = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        f{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            f{k} = cell2mat(file.f(1,l));
-            k = k+1;
+            f{l}{n} = cell2mat(file.f(1,l));
         end
     end
     save('CYG_f.mat', 'f', '-v7.3');
     clear f
     
-    k = 1;
-    time_ = cell(2*nSpw*nSpw, 1);
+    time_ = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        time_{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            time_{k} = cell2mat(file.time_(1,l)); % directly concatenated, or not?
-            k = k+1;
+            time_{l}{n} = cell2mat(file.time_(1,l)); % directly concatenated, or not?
         end
     end
     save('CYG_time.mat', 'time_', '-v7.3');
     clear time_
     
-    k = 1;
-    pos = cell(2*nSpw*nSpw, 1);
+    pos = cell(2*nSpw, 1);
     for l = 1:2*nSpw
+        pos{l} = cell(nSpw, 1);
         for n = 1:nSpw
             filename = [visibility_file_name, num2str(n), '.mat'];
             file = matfile(filename);  
-            pos{k} = cell2mat(file.pos(1,l));
-            k = k+1;
+            pos{l}{n} = cell2mat(file.pos(1,l));
         end
     end
     save('CYG_pos.mat', 'pos', '-v7.3');
@@ -199,7 +195,7 @@ end
 
 %% adapt to DR (see codes from Ming / ask Ming directly if necessary)
 %%
-ch = [1 : numel(y)];
+ch = [1 : nChannels];
 % output from the definition of the Fourier reduction scheme
 H = cell(nChannels, 1);  % holographic matrices
 yT = cell(nChannels, 1); % reduced data
@@ -211,7 +207,7 @@ u = cell(nChannels, 1);
 v = cell(nChannels, 1);
 uvidx = cell(nChannels, 1);
 
-for i = ch
+for i = ch % loop over the 32 channels
     
     i
     
@@ -223,97 +219,85 @@ for i = ch
     file_nWw = matfile('CYG_nWw.mat');
     
     %% compute weights
-    uw = cell2mat(file_uw.uw(i, 1));
-    vw = cell2mat(file_vw.vw(i, 1));
-    pos = cell2mat(file_pos.pos(i, 1));
-    time_ = cell2mat(file_time.time_(i, 1));
-    %[aWw] = util_gen_preconditioning_matrix(uw, vw, param_precond); % see if still necessary
+    y = file_y.y(i, 1); % to be verified
+    uw = file_uw.uw(i, 1);
+    vw = file_vw.vw(i, 1);
+    pos = file_pos.pos(i, 1);
+    time_ = file_time_.time_(i, 1);
     
     % set the blocks structure
     if param_block_structure.use_manual_partitioning == 1
         param_block.size = 90000; length(uw);
         param_block.snapshot = 0;
-        param_block.pos = pos;
-        out_block = util_time_based_block_sp_ar(uw,time_,param_block);
-        param_block_structure.partition = out_block.partition;
     end
     
     u{i} = [];
     v{i} = [];
     nW{i} = [];
     uvidx{i} = [];
-    for k = 1:numel(uw)
+    
+    for k = 1:numel(nSpw) % concatenate all the blocks coming from the 16 consecutive channels (loop over the 16 channels)
         % set the blocks structure (per frequency) [is aW{i} necessary?]
-        [u1, v1, ~, uvidx1, aW, nW1] = util_gen_block_structure(uw{k}, vw{k}, aWw{k}, nWw{k}, param_block_structure);
+        param_block.pos = pos{1}{k};
+        out_block = util_time_based_block_sp_ar(uw{1}{k},time_{1}{k},param_block);
+        param_block_structure.partition = out_block.partition;
+        aWw = util_gen_preconditioning_matrix(uw{1}{k}, vw{1}{k}, param_precond); % see if still necessary: needs to be kept in memory for DR? (no a priori)
+        [u1, v1, ~, uvidx1, aW, nW1] = util_gen_block_structure(uw{1}{k}, vw{1}{k}, aWw, nWw{1}{k}, param_block_structure);
         
         % reorder the blocks along a single dimension (remove intermediate level of the frequencies)
         u{i} = [u{i}; u1];
         v{i} = [v{i}; v1];
         nW{i} = [nW{i}; nW1];
         uvidx{i} = [uvidx{i}; uvidx1];
-    end
-    
-    % measurement operator initialization
-    fprintf('Initializing the NUFFT operator\n\n');
-    tstart = tic;
-   
-    %     for k = 1 : length(nW)
-    %         nW{k} = ones(size(nW{k}));
-    %     end
-    
-    %[A, At, G{i}, W{i}, Gw{i}] = op_p_nufft([v u], [Ny Nx], [Ky Kx], [oy*Ny ox*Nx], [Ny/2 Nx/2], nW);
-    % do I need to keep all the G matrices? (to be determined later on)
-    %[A, At, G{i}, W{i}] = op_p_nufft([v u], [Ny Nx], [Ky Kx], [oy*Ny ox*Nx], [Ny/2 Nx/2], nW);
-    [A, At, G, ~] = op_p_nufft([v u], [Ny Nx], [Ky Kx], [oy*Ny ox*Nx], [Ny/2 Nx/2], nW);
-    
-    %% --- Fourier reduction --- %%
-    H{i} = cell(numel(G), 1);
-    T{i} = cell(numel(G), 1);
-    yT{i} = cell(numel(G), 1);
-    Wm{i} = cell(numel(G), 1);
-    for j = 1:numel(G)
         
-        H{i}{j} = (G{j}')*G{j};
+        % measurement operator initialization
+        fprintf('Initializing the NUFFT operator\n\n');
         
-        % estimate threshold
-        dirty2 = norm(operatorPhit(y{i}{j}, G{j}', At) / sqrt(N));
+        %% --- Fourier reduction --- %%
+        H{i} = cell(numel(u1), 1); % one structure per block
+        T{i} = cell(numel(u1), 1);
+        yT{i} = cell(numel(u1), 1);
+        Wm{i} = cell(numel(u1), 1);
         
-        ... define Mask with the appropriate threshold -> W (how is this supposed to be done on real data?)
-        % no longer invoke function fourierReduction to reduce lambda functions
-        % fast way of matrix probing (using psf)
-        dirac2D = zeros(Ny, Nx);
-        dirac2D(ceil((Ny+1)/2), ceil((Nx+1)/2)) = 1;
-        PSF = operatorIpsf(dirac2D, A, At, H{i}{j}, [oy*Ny, ox*Nx]);
-        covariancemat = FT2(PSF);
-        d_mat = abs(real(covariancemat(:)));
-        %
-        if param_fouRed.enable_klargestpercent
-            Mask = (d_mat >= prctile(d_mat,100-param_fouRed.klargestpercent));
-        elseif param_fouRed.enable_estimatethreshold
-            % Embed the noise
-            noise1 = param_fouRed.sigma_noise * (randn(size(G{j}, 1),1) + 1j * randn(size(Gw{i}, 1), 1));
-            rn = FT2(At(G{j}'*noise1));  % Apply F Phi
-            th_dirty = param_fouRed.gamma * std(rn(:)) / dirty2;
-            fprintf('\nThe estimate threshold using ground truth is %e \n', th);
-            Mask = (d_mat >= th_dirty);
+        for j = 1:numel(u1) % loop over the blocks for the current channel
+            [A, At, G, ~] = op_p_nufft([{v1{j}} {u1{j}}], [Ny Nx], [Ky Kx], [oy*Ny ox*Nx], [Ny/2 Nx/2], {nW1{j}}); 
+            % this function can take cell inputs (but no need to keep multiple G matrices in memory)
+            H{i}{j} = (G{1}')*G{1};
+        
+            % estimate threshold
+            dirty2 = norm(operatorPhit(y{1}{k}(uvidx1{j}), G{1}', At) / sqrt(N));
+
+            % no longer invoke function fourierReduction to reduce lambda functions
+            % fast way of matrix probing (using psf)
+            dirac2D = zeros(Ny, Nx);
+            dirac2D(ceil((Ny+1)/2), ceil((Nx+1)/2)) = 1;
+            PSF = operatorIpsf(dirac2D, A, At, H{i}{j}, [oy*Ny, ox*Nx]);
+            covariancemat = FT2(PSF);
+            d_mat = abs(real(covariancemat(:)));
+            clear covariancemat
+            %
+            if param_fouRed.enable_klargestpercent
+                Mask = (d_mat >= prctile(d_mat,100-param_fouRed.klargestpercent));
+            elseif param_fouRed.enable_estimatethreshold
+                % Embed the noise
+                noise1 = param_fouRed.sigma_noise * (randn(size(G{1}, 1),1) + 1j * randn(size(G{1}, 1), 1));
+                rn = FT2(At(G{1}'*noise1));  % Apply F Phi
+                th_dirty = param_fouRed.gamma * std(rn(:)) / dirty2;
+                fprintf('\nThe estimate threshold using ground truth is %e \n', th);
+                Mask = (d_mat >= th_dirty);
+            end
+            d_mat = d_mat(Mask);
+            %
+            T{i}{j} = max(param_fouRed.diagthresholdepsilon, d_mat);  % This ensures that inverting the values will not explode in computation
+            T{i}{j} = 1./sqrt(T{i}{j});
+            Wm{i}{j} = Mask;
+
+            % reduce the data block
+            yT{i}{j} = dataReduce(y{1}{k}(uvidx1{j}), G{1}', At, T{i}{j}, Wm{i}{j});
         end
-        d_mat = d_mat(Mask);
-        %
-        T{i}{j} = max(param_fouRed.diagthresholdepsilon, d_mat);  % This ensures that inverting the values will not explode in computation
-        T{i}{j} = 1./sqrt(T{i}{j});
-        Wm{i}{j} = Mask;
-        
-        % reduce the data (per block)
-        yT{i}{j} = dataReduce(y{i}(uvidx{j}), G{j}', At, T{i}{j}, W{i}{j}); % to be adapted
     end
-    % missing: definition of epsilon, yT
+    % missing: definition of epsilon... [to be checked with Ming]
     %% ---
-    
-%     % block the data
-%     yb{i} = cell(length(u),1);
-%     for j = 1 : length(u) 
-%         yb{i}{j} = y{i}(uvidx{j});
-%     end
     
 end
 
