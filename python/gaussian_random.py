7f4463b64ae3b4d3f74f3fbd0da37b323a1315c0
CEA-jiangming <ming.jiang@epfl.ch>
2019-08-05 14:15:55 +0200

    python script to simulate ground truth

---
+++ python/gaussian_random.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fftIndgen(n):
+    a = np.arange(0, n//2)
+    b = np.arange(1, n//2+1)
+    b = b[::-1]
+    b = [-i for i in b]
+    return a + b
+
+
+def gaussian_random_field(Pk=lambda k: k**(-3.0), size=100):
+    def Pk2(kx, ky):
+        if kx == 0 and ky == 0:
+            return 0.0
+        return np.sqrt(Pk(np.sqrt(kx**2 + ky**2)))
+    noise = np.fft.fft2(np.random.normal(size=(size, size)))
+    amplitude = np.zeros((size, size))
+    for i, kx in enumerate(fftIndgen(size)):
+        for j, ky in enumerate(fftIndgen(size)):
+            amplitude[i, j] = Pk2(kx, ky)
+    return np.fft.ifft2(noise * amplitude)
+
+
+for alpha in [-4.0, -3.0, -2.0]:
+    out = gaussian_random_field(Pk=lambda k: k**alpha, size=256)
+    plt.figure()
+    plt.imshow(out.real, cmap='jet')
+
+plt.show()
+
