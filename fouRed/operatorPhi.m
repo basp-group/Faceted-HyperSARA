2be1db585d8b412620fae0b4ef63fe47ccdd53f3
CEA-jiangming <ming.jiang@epfl.ch>
2019-11-21 15:12:57 +0100

    clean fouRed folder

--- fouRed/operatorPhi.m
+++
@@ -1,8 +0,0 @@
-function y = operatorPhi(x, G, A, W)
-% This function implements the operator Phi = G * A
-% Real -> Complex
-x1 = A(real(x));
-if exist('W', 'var')
-    x1 = x1(W);
-end
-y = G * x1;
--- fouRed/operatorR.m
+++
@@ -1,7 +0,0 @@
-function y = operatorR(x, Phi_t, Sigma, Mask)
-% the embeddding operator R = \sigma * S * F * Phi^T
-% Complex -> Complex
-FT2 = @(x) fftshift(fft2(ifftshift(x)));
-im = FT2(Phi_t(x));
-im = im(:);
-y = Sigma .* im(Mask);
