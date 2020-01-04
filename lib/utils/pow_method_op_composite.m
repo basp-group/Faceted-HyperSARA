36f5af22588267d8b87df0a3efe719c57e5ad2bd
pthouvenin <pierreantoine.thouvenin@gmail.com>
2020-01-04 19:35:00 +0100

    Add power_method with composite input

---
+++ lib/utils/pow_method_op_composite.m
@@ -0,0 +1,56 @@
+function val = pow_method_op_composite(A, At, Q, K, nchannel_per_worker, im_size)
+%Computes the maximum eigen value of the compund 
+%operator AtA
+%
+% > A       composite lambda function (to be defined as a constant on each worker)
+% > At      composite lambda function for the full operator (per channel)
+% > Q       number of facets (workers)
+% > K       number of data nodes
+% > nchannel_per_worker  number of channels per data worker [K, 1]
+% > im_size image size (spatial dimensions) [2, 1]
+%
+% P.-A. Thouvenin, [04/01/2020]
+
+p = 1 + 10^(-6);
+pnew = 1;
+epsilon = 10^(-8);
+n = 1;
+nmax = 200;
+cond = abs(pnew - p) / pnew;
+
+spmd
+    if labindex > Q
+         % each worker owns a portion of the channels
+        x = randn([im_size, nchannel_per_worker(labindex-Q)]);
+        norm_x = norm(x(:))^2;
+        norm_x = gplus(norm_x);
+        norm_x = sqrt(norm_x);
+    end
+end
+
+% Iterations
+while (cond >= epsilon && n < nmax)
+    spmd
+        if labindex > Q 
+           xnew = At(A(x/norm_x)); % input normalised
+                    
+           squared_norm_diff = norm(xnew(:) - x(:))^2;
+           squared_norm_diff = gplus(squared_norm_diff, Q+1); % needed only on one worker
+           
+           x = xnew;
+           norm_x = norm(x(:))^2;
+           norm_x = gplus(norm_x);
+           norm_x = sqrt(norm_x);
+        end
+    end
+    
+    cond = sqrt(squared_norm_diff{Q+1}) / norm_x{Q+1};
+    n = n+1 ;
+    
+    display(['norm it n=' num2str(n) ' norm=' num2str(norm(xnew(:)))])
+end
+
+val = norm_x{Q+1}; % output not normalised
+
+end
+
