9d11bf01dacb10b034336b15c57efdba588ea94b
pthouvenin <pierreantoine.thouvenin@gmail.com>
2020-10-06 15:02:11 +0200

    Temporary update for debugging (remove exclusive node access)

--- job_py.py
+++ job_py.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import csv, subprocess, os
+import csv, subprocess
+from distutils.dir_util import mkpath
 
 parameter_file_full_path = "job_params.csv"
 
@@ -10,7 +11,7 @@ with open(parameter_file_full_path, "r") as csvfile:
 
     for job in reader:
 
-        os.mkdir("results/{0}/logs".format(job[0]))
+        mkpath("results/{0}/logs".format(job[0])) # if the directory already exists, do nothing
 
         slurm_command = """sbatch --job-name={0} --ntasks-per-node={1} \
             -e /lustre/home/sc004/pthouven/Faceted-Hyper-SARA/results/{0}/logs/{0}_L={5}_Qx={2}_Qy={3}_Qc={4}_id={11}_overlap={12}_p={6}_snr={7}.err \
