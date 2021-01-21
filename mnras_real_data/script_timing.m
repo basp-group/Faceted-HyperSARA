ca06b08185e93fed7a9b542689fb954b172f2b1b
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-01-21 13:53:31 +0100

    Add slurm script to retrieve timing

--- mnras_real_data/script_timing.m
+++ mnras_real_data/script_timing.m
@@ -10,10 +10,11 @@ filename_pattern = @(ind) fullfile('/lustre/home/shared/sc004/mnras_faceted_corr
     ['fhs_cw_rd_triangular_Nx=2560_Ny=1536_L=30_Qx=5_Qy=3_Qc=1_overlap=512_ind=',...
      num2str(ind),'_gam0=0.01_gam=5e-06_rw=20.mat']);
 
-files = [2];
+files = [2:5, 7:16];
 nfiles = numel(files);
 results_filename = cell(nfiles, 1);
-output_filename = '/lustre/home/shared/sc004/mnras_faceted_corrected/final_real_data/timing_faceted_hypersara.mat';
+% output_filename = '/lustre/home/shared/sc004/mnras_faceted_corrected/final_real_data/timing_faceted_hypersara.mat';
+output_filename = 'timing_faceted_hypersara.mat';
 
 for id = 1:nfiles % 1:nfile
     results_filename{id} = filename_pattern(files(id));
