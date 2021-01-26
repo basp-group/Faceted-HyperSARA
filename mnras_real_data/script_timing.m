a5397fdb1c0502b2fe1607b9d96615dc60a9d0f4
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-01-26 16:36:18 +0100

    Update script to retrieve metrics (w/o DR)

--- mnras_real_data/script_timing.m
+++ mnras_real_data/script_timing.m
@@ -7,12 +7,13 @@ addpath ../lib/utils
 %      num2str(ind),'_gam0=0.01_gam=5e-06_rw=20.mat']);
 
 %% Faceted HyperSARA
+res_path = '/lustre/home/shared/sc004/mnras_faceted_corrected/final_real_data/res_FacetedHyperSARA_4-8GHz_NEW.fits';
 filename_pattern = @(ind) fullfile('/lustre/home/shared/sc004/mnras_faceted_corrected/final_real_data', ...
     ['fhs_cw_rd_triangular_Nx=2560_Ny=1536_L=30_Qx=5_Qy=3_Qc=1_overlap=512_ind=',...
      num2str(ind),'_gam0=0.01_gam=5e-06_rw=20.mat']);
 
 nfacets = 15; % Qy = 3, Qx = 5, ncores_data = 15
-files = [2:5, 7:16]; % [1:16];
+files = 1:16; % [2:5, 7:16];
 Qc = numel(files);   % number of spectral sub-cubes
 results_filename = cell(Qc, 1);
 % output_filename = '/lustre/home/shared/sc004/mnras_faceted_corrected/final_real_data/timing_faceted_hypersara.mat';
@@ -25,18 +26,54 @@ end
 [aruntime, vruntime, acpu_time, vcpu_time, total_runtime, total_cpu_time, ... 
     iteration_number] = get_timing_facetedHypersara(results_filename, output_filename, nfacets);
 
-if Qc < 16 % if all sub-cubes not available
-    % estimate total cpu time based on number of iterations and average
-    % time per iteration per subcube
-    tcpu = total_cpu_time + (16-Qc)*(iteration_number/Qc)*acpu_time;
-      
-    fprintf("Qc = %i, iteration_number = %i \n", ...
+fprintf("HyperSARA: Qc = %i, iteration_number = %i \n", ...
         Qc, iteration_number/Qc)
-    fprintf(" aruntime (s) = %.2f, std_runtime (s) = %1.2e, acpu_time (s) = %.2f, std_cpu_time (s) = %1.2e \n", ...
-        aruntime, sqrt(vruntime), acpu_time, sqrt(vcpu_time));
-    fprintf(" total_runtime (h) = %2.2f, total_cpu_time (h) = %2.2f \n", ...
-        total_runtime/3600, tcpu/3600)
-end
+fprintf(" aruntime (s) = %.2f, std_runtime (s) = %1.2e, acpu_time (s) = %.2f, std_cpu_time (s) = %1.2e \n", ...
+    aruntime, sqrt(vruntime), acpu_time, sqrt(vcpu_time));
+fprintf(" total_runtime (h) = %2.2f, total_cpu_time (h) = %2.2f \n", ...
+    total_runtime/3600, tcpu/3600)
+
+% load full (normalised) residual cube
+res = fitsread(res_path);
+res = flipud(res);
+
+kurtosis_res = squeeze(kurtosis(res, 0, [1,2]));
+akurtosis_res = mean(kurtosis_res);
+skurtosis_res = std(kurtosis_res);
+
+std_res = squeeze(std(res, 0, [1,2]));
+astd_res = mean(std_res);
+sstd_res = std(std_res);
+
+skew_res = squeeze(skewness(res,0,[1,2]));
+askew_res = mean(skew_res);
+sskew_res = std(skew_res);
+
+save("metric_fhs.mat", '-v7.3', 'kurtosis_res', 'akurtosis_res', 'skurtosis_res', ...
+    'std_res', 'astd_res', 'sstd_res', 'skew_res', 'askew_res', 'sskew_res', ...
+    'aruntime', 'vruntime', 'acpu_time', 'vcpu_time', ...
+    'iteration_number', ...
+    'total_runtime', 'total_cpu_time')
+
+% Print results
+fprintf("astd_res = %1.2e, sstd_res = %1.2e, akurt = %1.2e, skurt = %1.2e, askew = %1.2e, sskew = %1.2e \n", ...
+   astd_res, sstd_res, akurtosis_res, skurtosis_res, askew_res, sskew_res)
+clear res
+
+
+% This part was only needed when interpolating results (missing cubes)
+% if Qc < 16 % if all sub-cubes not available
+%     % estimate total cpu time based on number of iterations and average
+%     % time per iteration per subcube
+%     tcpu = total_cpu_time + (16-Qc)*(iteration_number/Qc)*acpu_time;
+      
+%     fprintf("Qc = %i, iteration_number = %i \n", ...
+%         Qc, iteration_number/Qc)
+%     fprintf(" aruntime (s) = %.2f, std_runtime (s) = %1.2e, acpu_time (s) = %.2f, std_cpu_time (s) = %1.2e \n", ...
+%         aruntime, sqrt(vruntime), acpu_time, sqrt(vcpu_time));
+%     fprintf(" total_runtime (h) = %2.2f, total_cpu_time (h) = %2.2f \n", ...
+%         total_runtime/3600, tcpu/3600)
+% end
 
 %% SARA (only placeholders for now)
 
