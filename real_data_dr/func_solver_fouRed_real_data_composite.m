98e81ace4a34b9a28333f44a1f4f5936ad00bd64
Ming Jiang <ming.jiang@epfl.ch>
2021-02-02 11:22:46 +0800

    fix some typos

--- real_data_dr/func_solver_fouRed_real_data_composite.m
+++ real_data_dr/func_solver_fouRed_real_data_composite.m
@@ -226,7 +226,7 @@ if flag_algo == 1
     param_HSI.adapt_eps_rel_val = 5e-4; % bound on the relative change of the solution
     param_HSI.adapt_eps_change_percentage = 0.5*(sqrt(5)-1); % the weight of the update w.r.t the l2 norm of the residual data
 
-    param_HSI.reweight_alpha = 1; %1; % the parameter associated with the weight update equation and decreased after each reweight by percentage defined in the next parameter
+    param_HSI.reweight_alpha = (0.8)^10; %1; % the parameter associated with the weight update equation and decreased after each reweight by percentage defined in the next parameter
     param_HSI.reweight_alpha_ff = 0.8;
     param_HSI.total_reweights = 50; % -1 if you don't want reweighting
     param_HSI.reweight_abs_of_max = Inf; % (reweight_abs_of_max * max) this is assumed true signal and hence will have weights equal to zero => it wont be penalised
