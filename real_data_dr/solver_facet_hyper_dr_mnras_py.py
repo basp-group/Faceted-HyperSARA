3f4efabda83d8ebf35a85495bb18d7556f9a3dde
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-03-23 15:24:27 +0100

    Fix submission scripts dr

--- real_data_dr/solver_facet_hyper_dr_mnras_py.py
+++ real_data_dr/solver_facet_hyper_dr_mnras_py.py
@@ -14,9 +14,15 @@ gamma = 5e-6
 subInd = "[1:16]"
 fouRed_gamma = 15
 adpteps = 0
+computelowerbounds = 1 # only needed the first time (saved once it has been computed, loaded whenever the flag is 0)
+
 primal = 0
 homotopy = 0
-computelowerbounds = 1
+
+# experiments to be carried out:
+# primal = 0, homotopy = 0
+# primal = 1, homotopy = 0
+# primal = 1, homotopy = 1
 
 # qsub_command = "sbatch -J solver_facet_hyper_dr_mnras_gamma0{5}_gamma{6} --export datadir='{0}',name='{1}',Qx={2},Qy={3},Qc={4},gamma0={5},gamma={6},subInd={7},fouRed_gamma={8},adpteps={9} solver_facet_hyper_dr_composite_mnras.slurm".format(datadir, name, Qx, Qy, Qc, gamma0, gamma, subInd, fouRed_gamma, adpteps)
 
@@ -24,7 +30,7 @@ computelowerbounds = 1
 # qsub_command = "sbatch -J solver_facet_hyper_dr_mnras_gamma0{5}_gamma{6} --export datadir='{0}',name='{1}',Qx={2},Qy={3},Qc={4},gamma0={5},gamma={6},subInd={7},fouRed_gamma={8},adpteps={9},initoption={10} solver_facet_hyper_dr_composite_mnras.slurm".format(datadir, name, Qx, Qy, Qc, gamma0, gamma, subInd, fouRed_gamma, adpteps,initoption)
 
 # new option
-qsub_command = "sbatch -J solver_facet_hyper_dr_mnras_gamma0{5}_gamma{6} --export datadir='{0}',name='{1}',Qx={2},Qy={3},Qc={4},gamma0={5},gamma={6},subInd={7},fouRed_gamma={8},adpteps={9},primal={10},homotopy={11} solver_facet_hyper_dr_composite_mnras.slurm".format(datadir, name, Qx, Qy, Qc, gamma0, gamma, subInd, fouRed_gamma, adpteps, primal, homotopy, computelowerbounds)
+qsub_command = "sbatch -J solver_facet_hyper_dr_mnras_gamma0{5}_gamma{6} --export datadir='{0}',name='{1}',Qx={2},Qy={3},Qc={4},gamma0={5},gamma={6},subInd={7},fouRed_gamma={8},adpteps={9},primal={10},homotopy={11},computelowerbounds={12} solver_facet_hyper_dr_composite_mnras.slurm".format(datadir, name, Qx, Qy, Qc, gamma0, gamma, subInd, fouRed_gamma, adpteps, primal, homotopy, computelowerbounds)
 
 print(qsub_command) # Uncomment this line when testing to view the qsub command
 
