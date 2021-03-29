7446929d54eb4082a42b20f76af36b90a8ad4c14
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-03-29 15:33:09 +0200

    Finalize slurm script for spatial faceting experiment

--- mnras_sim_new/mnras_spatial/job_spatial.py
+++ mnras_sim_new/mnras_spatial/job_spatial.py
@@ -12,20 +12,21 @@ with open(parameter_file_full_path, "r") as csvfile:
 
     for job in reader:
 
-        ncores = np.minimum(int(job[2])*int(job[3]) + int(job[10]) + 1, 36) # max number of cpus = 36
+        ncores = np.minimum(int(job[4])*int(job[5]) + int(job[18]) + 1, 36) # max number of cpus = 36
 
-        slurm_command = """sbatch --job-name={5}_{1} --ntasks-per-node={19} \
-            -e {5}_{0}_L={1}_Qx={2}_Qy={3}_Qc={4}_id={8}_overlap={9}_gamma={18}_rw={20}.err \
-            -o {5}_{0}_L={1}_Qx={2}_Qy={3}_Qc={4}_id={8}_overlap={9}_gamma={18}_rw={20}.out \
-            -v --export=ALL,imgname={0},nchannels={1},Qx={2},Qy={3},Qc={4},algoversion={5},wintype={6},ncdata={7},ind={8},overlapsize={9},nreweights={10},gencube={11},gencov={12},genvis={13},genundersampledcube={14},computenorm={15},solve={16},covpath={17},gam={18},ncpus={19},rw={20} \
-            run_fhs_mnras.slurm""".format(*job)
+        slurm_command = """sbatch --job-name=spatial_{1} --ntasks-per-node={22} \
+            -e {0}_{1}_L={2}_Qx={4}_Qy={5}_Qc={6}_id={3}_overlap={8}_gamma={10}_rw={11}.err \
+            -o {0}_{1}_L={2}_Qx={4}_Qy={5}_Qc={6}_id={3}_overlap={8}_gamma={10}_rw={11}.out \
+            -v --export=ALL,imgname={0},algoversion={1},nchannels={2},ind={3},Qx={4},Qy={5},Qc={6},wintype={7},overlapx={8},overlapy={9},gam={10},
+            nreweights={11},gencube={12},gencov={13},genvis={14},computenorm={15},solve={16},covpath={17},ncdata={18},rw={19},flaghomotopy={20},lowerbounds={21},ncpus={22} \
+            run_fhs_mnras.slurm""".format(*job,ncores)
 
-        # print(slurm_command) # Uncomment this line when testing to view the sbatch command
+        print(slurm_command) # Uncomment this line when testing to view the sbatch command
 
         # Comment the following 3 lines when testing to prevent jobs from being submitted
-        exit_status = subprocess.call(slurm_command, shell=True)
-        if exit_status is 1:  # Check to make sure the job submitted
-            print("Job {0} failed to submit".format(slurm_command))
+        # exit_status = subprocess.call(slurm_command, shell=True)
+        # if exit_status is 1:  # Check to make sure the job submitted
+        #     print("Job {0} failed to submit".format(slurm_command))
 
         time.sleep(5)
 
