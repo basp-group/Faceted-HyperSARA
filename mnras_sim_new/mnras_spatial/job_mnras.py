ba598e480709b0cd498fb7ab630525fc76c74e07
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-03-29 17:47:36 +0200

    Fixing

--- mnras_sim_new/mnras_spatial/job_mnras.py
+++ mnras_sim_new/mnras_spatial/job_mnras.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv, subprocess, os
+import numpy as np
 import time
 
 parameter_file_full_path = "job_test.csv" #"job_params_mnras.csv"
@@ -11,11 +12,13 @@ with open(parameter_file_full_path, "r") as csvfile:
 
     for job in reader:
 
-        slurm_command = """sbatch --job-name={1} \
+        ncores = np.minimum(int(job[4])*int(job[5]) + int(job[18]) + 1, 36) # max number of cpus = 36
+
+        slurm_command = """sbatch --job-name=spatial_{1} --cpus-per-task={21} \
             -e {0}_{1}_L={2}_Qx={4}_Qy={5}_Qc={6}_id={3}_overlap={8}_gamma={10}_rw={11}.err \
             -o {0}_{1}_L={2}_Qx={4}_Qy={5}_Qc={6}_id={3}_overlap={8}_gamma={10}_rw={11}.out \
-            -v --export=ALL,imgname={0},algoversion={1},nchannels={2},ind={3},Qx={4},Qy={5},Qc={6},wintype={7},overlapx={8},overlapy={9},gam={10},
-            nreweights={11},gencube={12},gencov={13},genvis={14},computenorm={15},solve={16},covpath={17},ncdata={18},rw={19},flaghomotopy={20},lowerbounds={21},ncpus={22} \
+            -v --export=ALL,imagename={0},algoversion={1},nchannels={2},ind={3},Qx={4},Qy={5},Qc={6},wintype={7},overlapx={8},overlapy={9},gam={10},
+            nreweights={11},gencube={12},genvis={13},computenorm={14},solve={15},covpath={16},ncdata={17},rw={18},flaghomotopy={19},lowerbounds={20} \
             run_fhs_mnras.slurm""".format(*job)
 
         # print(slurm_command) # Uncomment this line when testing to view the sbatch command
