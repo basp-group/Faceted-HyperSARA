36a0ea811514c3b94f3a251e6af04950de32efc0
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-04-20 16:30:42 +0200

    Update parameters

--- mnras_sim_new/mnras_spatial/job_spatial.py
+++ mnras_sim_new/mnras_spatial/job_spatial.py
@@ -28,11 +28,11 @@ ind = 1
 Qc = 1
 rw = -1
 gam = '1'  # multiplicative factor affecting the ratio -> '1e-5' order of magnitude
-nreweights = 1
+nreweights = 30
 wintype = 'triangular'
 covpath = '../../data/vla_7.95h_dt10s.uvw.mat'
 ncdata = 20
-flaghomotopy = 0
+flaghomotopy = 1
 rw_type = 'dirty' # heuristic, dirty
 
 params = [imagename,algoversion,nchannels,ind,Qc,rw,gam,nreweights,wintype,covpath,ncdata,flaghomotopy,gencube,genvis,computenorm,lowerbounds,solve]
