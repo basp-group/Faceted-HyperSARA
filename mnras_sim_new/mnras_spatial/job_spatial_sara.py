09bce8c810f7bac3dc8376d75d95891a6a2d171b
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-04-23 21:47:54 +0200

    Fixing sara python

--- mnras_sim_new/mnras_spatial/job_spatial_sara.py
+++ mnras_sim_new/mnras_spatial/job_spatial_sara.py
@@ -44,15 +44,15 @@ for cubeid in range(1,nchannels+1):
     slurm_command = r"""sbatch --job-name=spatial_{1} --ntasks-per-node={21} \
     -e {0}_{1}_L={2}_Qx={16}_Qy={17}_Qc={3}_id={20}_overlapx={18}_overlapy={19}_gamma={5}_rw={4}.err \
     -o {0}_{1}_L={2}_Qx={16}_Qy={17}_Qc={4}_id={3}_overlapx={18}_overlapy={19}_gamma={5}_rw={4}.out \
-    -v --export=ALL,imagename={0},algoversion={1},nchannels={2},ind={20},Qx={16},Qy={17},Qc={3},wintype={7},overlapx={18},overlapy={19},gam={5},nreweights={6},gencube={11},genvis={12},computenorm={13},solve={15},covpath={8},ncdata={9},rw={4},flaghomotopy={10},lowerbounds={14},gambar=1,exptype={21},rwtype={22} \
+    -v --export=ALL,imagename={0},algoversion={1},nchannels={2},ind={20},Qx={16},Qy={17},Qc={3},wintype={7},overlapx={18},overlapy={19},gam={5},nreweights={6},gencube={11},genvis={12},computenorm={13},solve={15},covpath={8},ncdata={9},rw={4},flaghomotopy={10},lowerbounds={14},gambar=1,exptype={22},rwtype={23} \
     run_fhs_mnras.slurm""".format(*params,cubeid,ncores,exp_type,rw_type)
 
-    # print(slurm_command) # Uncomment this line when testing to view the sbatch command
+    print(slurm_command) # Uncomment this line when testing to view the sbatch command
 
     # Comment the following 3 lines when testing to prevent jobs from being submitted
-    exit_status = subprocess.call(slurm_command, shell=True)
-    if exit_status is 1:  # Check to make sure the job submitted
-        print("Job {0} failed to submit".format(slurm_command))
+    # exit_status = subprocess.call(slurm_command, shell=True)
+    # if exit_status is 1:  # Check to make sure the job submitted
+    #     print("Job {0} failed to submit".format(slurm_command))
 
     time.sleep(5)
 
