a8da6955f89b1c5bd682118a2d6770f423ef9ca6
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-05-03 09:20:59 +0200

    Fixing sara cube scripts

--- figures/get_sara_cube.m
+++ figures/get_sara_cube.m
@@ -9,7 +9,7 @@ function get_sara_cube(pathname, Ny, Nx, nChannels, gam, homotopy, rwtype, ...
     cubename = @(channel) strcat(...
         "x_cygASband_Cube_512_1024_20_sara_srf=2_none_Qy=1_Qx=1_Qc=20_", ...
         "gam=", num2str(gam), "_homotopy=1_rwtype=", rwtype, ...
-        "homotopy=", homotopy, "_snr=40");
+        "homotopy=", homotopy, "_snr=40.fits");
 
     % x_cygASband_Cube_512_1024_20_sara_srf=2_none_Qy=1_Qx=1_Qc=20_ind=7_gam=10_homotopy=1_rwtype=dirty_snr=40.fits
 
@@ -20,7 +20,7 @@ function get_sara_cube(pathname, Ny, Nx, nChannels, gam, homotopy, rwtype, ...
         x_ = fitsread(fullFilename);
         x(:,:,l) = x_;
     end
-    fitswrite(x, cubename);
+    fitswrite(x, fullfile(pathname,cubename));
 
     % evaluate full and per channel reconstruction snr
     x0 = fitsread(pathToGrountruth);
