f6d1737c0dac8bbe2097f56c32f475a022b7cb66
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-05-03 15:37:27 +0200

    Fixing

--- figures/get_sara_cube.m
+++ figures/get_sara_cube.m
@@ -14,7 +14,7 @@ function get_sara_cube(pathname, Ny, Nx, nChannels, gam, homotopy, rwtype, ...
 
     x = zeros(Ny, Nx, nChannels);
 
-    for l = 2:nChannels
+    for l = 1:nChannels
         disp(fullfile(pathname, strcat(filename(l), '.fits')));
         fullFilename = fullfile(pathname, strcat(filename(l), '.fits'));
         x_ = fitsread(fullFilename);
