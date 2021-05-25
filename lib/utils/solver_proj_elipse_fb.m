063bb4315b1815e913f9c5a2155291c54a104ed6
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-05-25 16:40:46 +0200

    Solve issue in descent step forward backward algo (ellipsoid projection)

--- lib/utils/solver_proj_elipse_fb.m
+++ lib/utils/solver_proj_elipse_fb.m
@@ -3,7 +3,7 @@ function [z, k] = solver_proj_elipse_fb(v2, r2, y, U, epsilont, z0, max_itr, min
 sc = @(z, radius) z * min(radius/norm(z(:)), 1);
 z = z0;
 alpha = v2 + r2;
-mu = 1/max(U)^2;
+mu = 1.99/max(abs(U));
 zdelta = inf;
 k = 0;
 while k < min_itr || (k < max_itr && zdelta > eps)
