1c030153674d291bdf5532fc56aa4b4beef3416a
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-06-14 10:00:25 +0200

    [fix] Correct variable name in Python submission script (sara)

--- mnras_sim_new/mnras_spectral/job_spectral_sara.py
+++ mnras_sim_new/mnras_spectral/job_spectral_sara.py
@@ -26,18 +26,18 @@ algoversion = 'sara'
 nchannels = 100
 Qc = nchannels
 rw = -1
-gam = '1'
+gam = '3'
 nreweights = 30
 wintype = 'none'
 covpath = '../../data/msSpecs.mat' # '../../data/vla_7.95h_dt10s.uvw256.mat'
 ncdata = 9 # number of workers in this case (one per dictionary)
 flaghomotopy = 0
 exp_type = 'spectral'
-rw_type = 'dirty' # 'ground_truth' 'dirty' 'heuristic'
+rw_type = 'heuristic' # 'ground_truth' 'dirty' 'heuristic'
 superresolution_factor = 2
 isnr = 40
 updatereg = 0
-regtype = 'inv' # 'inv' 'log' 'heuristic'
+regtype = 'heuristic' # 'inv' 'log' 'heuristic'
 xapprox = 'none' # 'none' 'precond'
 noise_transfer = 'none' # 'none' 'precond'
 reg_option = 'none' # 'none' 'dirty'
@@ -58,15 +58,15 @@ for cubeid in range(1,nchannels+1):
     slurm_command = r"""sbatch --job-name=spectral_{1} --ntasks-per-node={21} \
     -e {27}/{0}_{1}_L={2}_Qx={16}_Qy={17}_Qc={3}_id={20}_overlapx={18}_overlapy={19}_gamma={5}_rw={4}_rwt={23}_exptype={22}_srf={24}_snr={25}_homotopy={10}_updatereg={26}_regtype={28}_xapprox={29}_nt={30}_ropt={32}.err \
     -o {27}/{0}_{1}_L={2}_Qx={16}_Qy={17}_Qc={3}_id={20}_overlapx={18}_overlapy={19}_gamma={5}_rw={4}_rwt={23}_exptype={22}_srf={24}_snr={25}_homotopy={10}_updatereg={26}_regtype={28}_xapprox={29}_nt={30}_ropt={32}.out \
-    -v --export=ALL,imagename={0},algoversion={1},nchannels={2},ind={20},Qx={16},Qy={17},Qc={3},wintype={7},overlapx={18},overlapy={19},gam={5},nreweights={6},gencube={11},genvis={12},computenorm={13},solve={15},covpath={8},ncdata={9},rw={4},flaghomotopy={10},lowerbounds={14},gambar=1,exptype={22},rw_type={23},superresolution={24},snr={25},updatereg={26},regtype={28},xapprox={29},noisetransfer={30},logpath={31},regoption={32} \
+    -v --export=ALL,imagename={0},algoversion={1},nchannels={2},ind={20},Qx={16},Qy={17},Qc={3},wintype={7},overlapx={18},overlapy={19},gam={5},nreweights={6},gencube={11},genvis={12},computenorm={13},solve={15},covpath={8},ncdata={9},rw={4},flaghomotopy={10},lowerbounds={14},gambar=1,exptype={22},rwtype={23},superresolution={24},isnr={25},updatereg={26},regtype={28},xapprox={29},noisetransfer={30},logpath={31},regoption={32} \
     run_fhs_mnras.slurm""".format(*params,cubeid,ncores,exp_type,rw_type,superresolution_factor,isnr,updatereg,slurm_log_path,regtype,xapprox,noise_transfer,log_path,reg_option)
 
-    # print(slurm_command) # Uncomment this line when testing to view the sbatch command
+    print(slurm_command) # Uncomment this line when testing to view the sbatch command
 
     # Comment the following 3 lines when testing to prevent jobs from being submitted
-    exit_status = subprocess.call(slurm_command, shell=True)
-    if exit_status is 1:  # Check to make sure the job submitted
-        print("Job {0} failed to submit".format(slurm_command))
+    # exit_status = subprocess.call(slurm_command, shell=True)
+    # if exit_status is 1:  # Check to make sure the job submitted
+    #     print("Job {0} failed to submit".format(slurm_command))
 
     # time.sleep(0.5)
 
