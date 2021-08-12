211bfaae673e87e9ce04f1c28730ab4f445d42fc
not populated <adabbech@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2021-08-12 23:55:28 +0100

    updated logfile name

--- experiments/real/pyimaging.py
+++ experiments/real/pyimaging.py
@@ -30,12 +30,12 @@ def main():
    with open(parameter_file, "rt") as csvfile:
         reader = csv.reader(csvfile)
         for job in reader:
-            job_name="""{3}_{0}_ID{1}_Subcube-{2}""".format(*job)
+            job_name="""{3}_{0}_ID{1}_cube-{2}_gam-{4}_gambar-{5}""".format(*job)
             #log file
             log_file = """{logpath}/{jobname}""".format(jobname=job_name,logpath=log_path)
             
             # command to launch
-            sbatch_command = """sbatch --job-name {jobname} --error {log}.err --output {log}.out  --export=LOG={log}.log,PDIR={projectpath},GAM={4},GAMBAR={5},SUBCUBE={2},RUNID={1},RW={6},nRW={7},LOG={log}.log  {slurmFile}""".format(*job,jobname=job_name,slurmFile=slurm_file,log=log_file,projectpath=project_path_run) 
+            sbatch_command = """sbatch --job-name {jobname} --error {log}.err --output {log}.out  --export=LOG={log}.log,PDIR={projectpath},ALGO={3},IMAGENAME={0},GAM={4},GAMBAR={5},SUBCUBE={2},RUNID={1},RW={6},nRW={7},LOG={log}.log  {slurmFile}""".format(*job,jobname=job_name,slurmFile=slurm_file,log=log_file,projectpath=project_path_run) 
             
             print(" ")
             print(sbatch_command) # Uncomment this line when testing to view the sbatch command
