b7e15db0bd8095d0b3598504043736ea83d68e90
not populated <adabbech@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2021-08-14 02:26:37 +0100

    scripts to get subcubes

---
+++ experiments/real/real_data/gen_subcubes/split_range_interleaved.m
@@ -0,0 +1,30 @@
+function rg = split_range_interleaved(nchunks, N)
+% Tessellates 1:N into interleaved subsets.
+%
+% Tessellates 1:N into subsets of interleaved indices, each containing
+% approximately the same number of indices (downsampling of 1:N).
+%
+% Args:
+%     nchunks (int): number of segments.
+%     N (int): total number of indices. 
+%
+% Returns:
+%     rg (array): first/last index of each segment {nchunks, 1}.
+
+%-------------------------------------------------------------------------%
+%%
+% Code: P.-A. Thouvenin.
+%-------------------------------------------------------------------------%
+%%
+if nchunks > N
+    error('split_range_interleaved:ValueError', ...
+    'Number of facets nchunks=%i greater than the dimension N=%i', nchunks, N);
+end
+
+rg = cell(nchunks, 1);
+for q = 1:nchunks
+    rg{q} = q:nchunks:N;
+end
+
+end
+    
