ada44c47b3dfd4690cbcf577ef2f7cc9c43513ec
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-09-01 21:40:56 +0200

    [add] Complete missing documentation

--- lib/print/display_image.m
+++ lib/print/display_image.m
@@ -1,29 +1,60 @@
 function [f, h] = display_image(x, fig_size, shift_colorbar, clim, map_img, fontsize, bool_log, location)
+% Display the input image :math:`x`.
+%
+% Parameters
+% ----------
+% x : array
+%     Input image.
+% fig_size : array (int)
+%     Size of the figure (in pixels).
+% shift_colorbar : array
+%     Shift applied to the position of the colorbar.
+% clim : array
+%     Values associated to the limits of the colorbar.
+% map_img : array
+%     Colormap.
+% fontsize : int
+%     Size of the font.
+% bool_log : bool
+%     Flag to activate log scale for the displayed image.
+% location : string
+%     String specifynig the location of the colorbar within the figure.
+%
+% Returns
+% -------
+% f : handle
+%     Figure handle.
+% h : handle
+%     Colorbar handle.
+%
 
-    f=figure('visible','on');
-    % sets axes background
-    set(gca, 'Color', 'none');
-    set(f,'PaperOrientation',orient);
-    set(f,'units','pixel','outerposition',[0 0 fig_size])
-    
-    % t = tiledlayout(1,1,'Padding','none'); % 'none' before R2021, 'tight'after
-    % t.Units = 'inches';
-    % t.OuterPosition = [0.25 0.25 4 4];
-    % nexttile;
-    
-    imagesc(x, clim);
-    colormap(gca, map_img);
+%%
+f=figure('visible','on');
+% sets axes background
+set(gca, 'Color', 'none');
+set(f,'PaperOrientation',orient);
+set(f,'units','pixel','outerposition',[0 0 fig_size])
 
-    axis image
-    ax = gca;
-    ax.YAxis.Visible = 'off';
-    ax.XAxis.Visible = 'off';
-    ax.TickLabelInterpreter='latex';
-    if bool_log
-        set(ax,'ColorScale','log'); 
-    end
-    h = colorbar;
-    set(h,'Location', location); % 'southoutside'
-    set(h,'Fontsize',fontsize);
-    pause(0.5)
-    h.Position = h.Position + shift_colorbar;
+% t = tiledlayout(1,1,'Padding','none'); % 'none' before R2021, 'tight'after
+% t.Units = 'inches';
+% t.OuterPosition = [0.25 0.25 4 4];
+% nexttile;
+
+imagesc(x, clim);
+colormap(gca, map_img);
+
+axis image
+ax = gca;
+ax.YAxis.Visible = 'off';
+ax.XAxis.Visible = 'off';
+ax.TickLabelInterpreter='latex';
+if bool_log
+    set(ax,'ColorScale','log'); 
+end
+h = colorbar;
+set(h,'Location', location); % 'southoutside'
+set(h,'Fontsize',fontsize);
+pause(0.5)
+h.Position = h.Position + shift_colorbar;
+
+end
--- lib/print/display_zoom.m
+++ lib/print/display_zoom.m
@@ -1,43 +1,72 @@
-function [f, h] = display_zoom(x, fig_size, pixel_shift_colorbar, clim_log, map_img, fontsize, tickLabel)
+function [f, h] = display_zoom(x, fig_size, pixel_shift_colorbar, clim, cmap, fontsize, tickLabel)
+% Display a the input image :math:`x` (zoomed-in regions from the MNRAS 
+% paper).
+%
+% Parameters
+% ----------
+% x : array
+%     Input image.
+% fig_size : array (int)
+%     Size of the figure (in pixels).
+% pixel_shift_colorbar : array
+%     Shift applied to the position of the colorbar (in pixels).
+% clim : array
+%     Values associated to the limits of the colorbar.
+% cmap : array
+%     Colormap.
+% fontsize : int
+%     Size of the font.
+% tickLabel : array
+%     Values for the ticks reported on the colorbar.
+%
+% Returns
+% -------
+% f : handle
+%     Figure handle.
+% h : handle
+%     Colorbar handle.
+%
 
-    f=figure('visible','on');
-    % sets axes background
-    set(gca, 'Color', 'none');
-    set(f,'PaperUnits','centimeters')
-    set(f,'PaperType','A4');
-    set(f,'PaperOrientation',orient);
-    set(f,'units','pixel','outerposition',[0 0 fig_size])
-    im = imagesc(x, clim_log);
-    colormap(gca, map_img);
-    axis image
-    ax = gca;
-    ax.YAxis.Visible = 'off';
-    ax.XAxis.Visible = 'off';
-    ax.TickLabelInterpreter='latex';
-    h = colorbar;
-    set(h,'Fontsize',fontsize)
-    set(h,'color','white')
-    set(gca,'ColorScale','log');
-    set(h,'XTick',tickLabel);
-    pause(0.5)
-    % Get the new aspect ratio data
-    aspect = get(ax,'PlotBoxAspectRatio');
-    % Change axes Units property (this only works with non-normalized units)
-    set(ax,'Units','pixels');
-    % Get and update the axes width to match the plot aspect ratio.
-    pos = get(ax,'Position');
-    pos(3) = aspect(1)/aspect(2)*pos(4);
-    set(ax,'Position',pos);
-    pause(0.1)
-    set(h,'Units', 'pixels')
-    pos_h = get(h,'Position');
-    pos_h = [pos(1)+pos(3)+pixel_shift_colorbar, pos_h(2), 2*pos_h(3), pos(4)];
-    set(h,'Position',pos_h);
-    set(ax,'Units','normalized');
-    pause(0.1)
-    pos = get(ax,'Position');
-    annotation(f,'rectangle',...
-    [pos(1), pos(2), im.Parent.InnerPosition(3), pos(4)],...
-    'Color',[1 1 1],...
-    'LineWidth',4);
+%%
+f=figure('visible','on');
+% sets axes background
+set(gca, 'Color', 'none');
+set(f,'PaperUnits','centimeters')
+set(f,'PaperType','A4');
+set(f,'PaperOrientation',orient);
+set(f,'units','pixel','outerposition',[0 0 fig_size])
+im = imagesc(x, clim);
+colormap(gca, cmap);
+axis image
+ax = gca;
+ax.YAxis.Visible = 'off';
+ax.XAxis.Visible = 'off';
+ax.TickLabelInterpreter='latex';
+h = colorbar;
+set(h,'Fontsize',fontsize)
+set(h,'color','white')
+set(gca,'ColorScale','log');
+set(h,'XTick',tickLabel);
+pause(0.5)
+% Get the new aspect ratio data
+aspect = get(ax,'PlotBoxAspectRatio');
+% Change axes Units property (this only works with non-normalized units)
+set(ax,'Units','pixels');
+% Get and update the axes width to match the plot aspect ratio.
+pos = get(ax,'Position');
+pos(3) = aspect(1)/aspect(2)*pos(4);
+set(ax,'Position',pos);
+pause(0.1)
+set(h,'Units', 'pixels')
+pos_h = get(h,'Position');
+pos_h = [pos(1)+pos(3)+pixel_shift_colorbar, pos_h(2), 2*pos_h(3), pos(4)];
+set(h,'Position',pos_h);
+set(ax,'Units','normalized');
+pause(0.1)
+pos = get(ax,'Position');
+annotation(f,'rectangle',...
+[pos(1), pos(2), im.Parent.InnerPosition(3), pos(4)],...
+'Color',[1 1 1],...
+'LineWidth',4);
+
 end
