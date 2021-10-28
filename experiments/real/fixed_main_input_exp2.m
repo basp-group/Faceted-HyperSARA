b1591fe9e1af72648c04d3cc25dc594e186a9c06
Arwa Dabbech <dabbecharoua@gmail.com>
2021-10-28 14:45:33 +0100

    [update] exp2 scripts

--- experiments/real/fixed_main_input_exp2.m
+++ experiments/real/fixed_main_input_exp2.m
@@ -4,26 +4,26 @@ clear ; clc; close all; delete(gcp('nocreate'))
 % calib_dir = '/lustre/home/shared/sc004/FACETED_HYPERSARA_EXPERIMENTS/calib/SPWIN/'
 % cube_filename = '/lustre/home/shared/sc004/FACETED_HYPERSARA_EXPERIMENTS/data/CYG-win-'
 
-cirrus = 0;
-main_dir='/Users/ad33/CodesScience/FHS_EXP12/Faceted-Hyper-SARA/';
+cirrus =1 ;
+main_dir='//lustre/home/shared/sc004/FACETED_HYPERSARA_EXPERIMENTS/';
 calib_dir=[main_dir,'calib/SPWIN'];
-cube_filename=[main_dir, 'data/CYG/cyga_data_spwin_'];
-project_dir = [main_dir,'experiments/real'];
+cube_filename=[main_dir, 'data/cyga_data_spwin_'];
+project_dir = [main_dir,'Faceted-Hyper-SARA-EXP2/experiments/real'];
 
 %% general params
 % image details, dims &  cellsize
 param_global.Nx = 2560;%floor(2560*sqrt(2)) + mod(floor(2560*sqrt(2)),2);
 param_global.Ny = 1536;%floor(1536*sqrt(2)) + mod(floor(1536*sqrt(2)),2);
-param_global.pixelSize = 0.0424;%asec
+param_global.pixelSize =0.06; 0.0424;%asec
 %faceting params
-param_global.Qx =1;4*2;
-param_global.Qy =1;2*2;
+param_global.Qx =5;4*2;
+param_global.Qy =3;2*2;
 param_global.Qc =1;
-param_global.window_type="triangular";
+param_global.window_type='triangular';
 param_global.overlap_fraction =[0.5,0.5];
 %
 param_global.exp_type = 'realexp';
-param_global.main_dir ='../../';
+param_global.main_dir =[main_dir,'/Faceted-Hyper-SARA-EXP2/'];
 param_global.die_filename = @(spwin) strcat(calib_dir,'/dies/spwin',num2str(spwin),'_dies.mat');
 param_global.l2bounds_filename = @(spwin) strcat(calib_dir,'/l2bounds/spwin',num2str(spwin),'_l2bounds.mat');
 param_global.model_filename = @(spwin) strcat(calib_dir,'/model_image/spwin',num2str(spwin),'_model_image.fits');
@@ -39,14 +39,13 @@ input_flags.wprojection =0;
 
 % cd ([project_dir,filesep,'experiments',filesep,'real']);
 imagename = 'CygA'  ;
-ncoredata = 1  ; %30 for cyga
 
-spwins2image = [1 ];% [1:17 19 21:32]; %ids of the channels to be imaged from spwin=spwin_ind;
+spwins2image = 1;%[1 2 3 4 5 6];% [1:17 19 21:32]; %ids of the channels to be imaged from spwin=spwin_ind;
 %if 'sara', make sure to select the id of the channel to be imaged.
-% ncoredata = param_global.Qx *param_global.Qy+ numel(spwins2image); %30 for cyga
 
-if ~cirrus
-    algoversion='fhs';
+ncoredata =  numel(spwins2image); %30 for cyga
+
+    algoversion='sara';
     subcube_ind=1;
     param_reg.gam=1;
     param_reg.gam_bar=1;
@@ -54,4 +53,3 @@ if ~cirrus
     param_reg.nReweights=5;
     main_real_data_dev_exp2(imagename,cube_filename,subcube_ind,spwins2image,...
         algoversion,ncoredata,param_global,param_reg,input_flags,cirrus) ;    
-end
