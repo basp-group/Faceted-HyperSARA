7c4975c3dd455e75bf47ea185450e38bed74ca7e
pthouvenin <pierreantoine.thouvenin@gmail.com>
2021-11-01 18:55:07 +0100

    [fix] Finalize and debug data generations script based on json config

--- experiments/script_example/main_generate_data_.m
+++ experiments/script_example/main_generate_data_.m
@@ -1,36 +1,29 @@
-function main_generate_data(json_filename, ind)
+function main_generate_data_(json_name, image_name, ncores_data, coverage_path, ...
+    exp_type, superresolution_factor, isnr, flagDR, flag_cirrus, flag_generateCoverage)
+% %% Debug parameters
+% image_name = 'W28_512'; %'cygASband_Cube_H'; %'W28_512';
+% exp_type = 'local_test'; % 'spectral', 'spatial', 'test'
+% flag_generateCoverage = 0;
+% flagDR = 0;
+% 
+% ncores_data = 2; % number of cores assigned to the data fidelity terms (groups of channels)
+% coverage_path = "data/vla_7.95h_dt10s.uvw256.mat" ;%"data/msSpecs.mat"; % "data/vla_7.95h_dt10s.uvw256.mat";
+% isnr = 50;
+% 
+% superresolution_factor = 2;
+% flag_cirrus = false;
+% kernel = 'minmax:tuned'; % 'kaiser' (for real data), 'minmax:tuned'
 
 %%
 % TODO: update util_gen_measurement_operator to enable kaiser kernels
-% json_filename = "setup_matlab.json";
-% ind = 1;
 format compact;
 
 % seed for the random number generation (used only when generating data)
 seed = 0;
 
-% Read .json configuration file
-[speed_of_light, param_general, param_model, ~, param_nufft, ...
- param_blocking, param_precond, param_synth, ~] = ...
-    read_json_configuration(json_filename, ind);
-
-% %% Debug parameters
-% param_general.image_name = 'W28_512'; %'cygASband_Cube_H'; %'W28_512';
-% param_synth.exp_type = 'local_test'; % 'spectral', 'spatial', 'test'
-% param_synth.flag_generateCoverage = 0;
-% param_model.flagDR = 0;
-% 
-% param_model.ncores_data = 2; % number of cores assigned to the data fidelity terms (groups of channels)
-% param_general.path_uv_coverage = "data/vla_7.95h_dt10s.uvw256.mat" ;%"data/msSpecs.mat"; % "data/vla_7.95h_dt10s.uvw256.mat";
-% param_synth.isnr = 50;
-% 
-% param_synth.superresolution_factor = 2;
-% param_general.flag_cirrus = false;
-% param_nufft.kernel = 'minmax:tuned'; % 'kaiser' (for real data), 'minmax:tuned'
-
 disp('Synthetic data generation');
-disp(['Reference image: ', param_general.image_name]);
-disp(['Input SNR: ', num2str(param_synth.isnr)]);
+disp(['Reference image: ', image_name]);
+disp(['Input SNR: ', num2str(isnr)]);
 
 addpath ../../lib/operators/;
 addpath ../../lib/measurement-operator/nufft/;
@@ -44,46 +37,45 @@ addpath ../../src/;
 
 % setting paths to results and reference image cube
 data_path = '../../data/';
-results_path = fullfile('results', strcat(param_general.image_name, '_', param_synth.exp_type));
+results_path = fullfile('results', strcat(image_name, '_', exp_type));
 mkdir(data_path);
 mkdir(results_path);
+
+% read options from .json file
+
 
 %%
 % ! load the appropriate portion of the reference image cube
 % ! if spectral faceting is active, just load the interesting portion of 
 % ! the full image cube
-switch param_synth.exp_type
+switch exp_type
     case "spatial"
-        param_general.image_name = 'cygASband_Cube_1024_2048_20';
+        image_name = 'cygASband_Cube_1024_2048_20';
         spectral_downsampling = 1;
         spatial_downsampling = 1;
     case "spectral"
-        param_general.image_name = 'cygASband_Cube_256_512_100';
+        image_name = 'cygASband_Cube_256_512_100';
         spectral_downsampling = 1;
         spatial_downsampling = 1;
     case "test"
-        param_general.image_name = 'cygASband_Cube_512_1024_20';
+        image_name = 'cygASband_Cube_512_1024_20';
         spectral_downsampling = 1;
         spatial_downsampling = 1;
     case "local_test"
-        param_general.image_name = 'cygASband_Cube_256_512_100';
+        image_name = 'cygASband_Cube_256_512_100';
         spectral_downsampling = 25;
         spatial_downsampling = 1;
-        param_general.path_uv_coverage = "data/vla_7.95h_dt10s.uvw256.mat";
-        param_model.ncores_data = 2;
-        param_general.flag_cirrus
+        coverage_path = "data/vla_7.95h_dt10s.uvw256.mat";
     case "old_local_test"
-        param_general.image_name = 'cubeW28';
+        image_name = 'cubeW28';
         spectral_downsampling = 20;
         spatial_downsampling = 4;
-        param_general.path_uv_coverage = "data/vla_7.95h_dt10s.uvw256.mat";
-        param_model.ncores_data = 2;
-        param_general.flag_cirrus
+        coverage_path = "data/vla_7.95h_dt10s.uvw256.mat";
     otherwise
         error("Unknown experiment type");
 end
 
-reference_cube_path = fullfile(data_path, strcat(param_general.image_name, '.fits'));
+reference_cube_path = fullfile(data_path, strcat(image_name, '.fits'));
 info        = fitsinfo(reference_cube_path);
 rowend      = info.PrimaryData.Size(1);
 colend      = info.PrimaryData.Size(2);
@@ -98,7 +90,7 @@ x0 = fitsread(reference_cube_path, 'primary', ...
 n_channels = floor(sliceend / spectral_downsampling);
 
 [Ny, Nx, nchans] = size(x0);
-input_snr = param_synth.isnr * ones(nchans, 1);  % input SNR (in dB)
+input_snr = isnr * ones(nchans, 1);  % input SNR (in dB)
 
 disp(['Number of channels considered: ', num2str(nchans)]);
 
@@ -114,28 +106,28 @@ clear spatial_downsampling spectral_downsampling;
 
 %% Auxiliary function needed to select the appropriate workers
 % index of channels from the subcube to be handled on each data worker
-rg_c = split_range(param_model.ncores_data, nchans);
+rg_c = split_range(ncores_data, nchans);
 
 % frequencies associated with the current subcube
 fmax = frequencies(end);
 
 %% Setup name of data file
 data_name_function = @(nchannels) strcat('y_', ...
-    param_synth.exp_type, '_', param_general.image_name, '_srf=', num2str(param_synth.superresolution_factor), ...
+    exp_type, '_', image_name, '_srf=', num2str(superresolution_factor), ...
     '_Ny=', num2str(Ny), '_Nx=', num2str(Nx), '_L=', num2str(nchannels), ...
-    '_snr=', num2str(param_synth.isnr), '.mat');
+    '_snr=', num2str(isnr), '.mat');
 
 data_name = data_name_function(n_channels);
 
 %% Define problem configuration (rng, nufft, preconditioning, blocking,
 % NNLS (epsilon estimation), SARA dictionary)
-% parameters_problem;
+parameters_problem;
 
 %% Generate/load uv-coverage
 % generating u-v coverage
 % ! reminder uv-coverage and weighting
 % https://casa.nrao.edu/Release4.1.0/doc/UserMan/UserMansu259.html
-if param_synth.flag_generateCoverage
+if flag_generateCoverage
     cov_type = 'vlaa';
     p = 0.5;
     dl = 1.1;
@@ -147,10 +139,10 @@ if param_synth.flag_generateCoverage
     [u, v, ~] = generate_uv_coverage(T, hrs, dl, cov_type);
     u = u(:) * frequencies(1) / fmax;
     v = v(:) * frequencies(1) / fmax;
-    fitswrite([u, v, ones(numel(u), 1)], param_general.path_uv_coverage);
-    disp(param_general.path_uv_coverage);
+    fitswrite([u, v, ones(numel(u), 1)], coverage_path);
+    disp(coverage_path);
 else
-    disp(strcat("Loading coverage: ", param_general.path_uv_coverage));
+    disp(strcat("Loading coverage: ", coverage_path));
     
     % VLA configuration
     % A. 762775 -> 3
@@ -158,8 +150,8 @@ else
     % C. 202957 -> 1
     % D. 47750 -> 0
 
-    if strcmp(param_synth.exp_type, "spectral")
-        load(param_general.path_uv_coverage, 'uvw', 'obsId');
+    if strcmp(exp_type, "spectral")
+        load(coverage_path, 'uvw', 'obsId');
         size(uvw);
         u1 = uvw(obsId == 2, 1) * fmax / speed_of_light;
         v1 = uvw(obsId == 2, 2) * fmax / speed_of_light;
@@ -167,11 +159,11 @@ else
     else
         % ! normalize u,v coverage w.r.t. the highest frequency (i.e., uv expressed in
         % units of the smallest wavelenght, associated with the highest frequency)
-        load(param_general.path_uv_coverage, 'uvw');
+        load(coverage_path, 'uvw');
         size(uvw);
         u1 = uvw(:, 1) * fmax / speed_of_light;
         v1 = uvw(:, 2) * fmax / speed_of_light;
-%         load(param_general.path_uv_coverage, 'uvw', 'obsId');
+%         load(coverage_path, 'uvw', 'obsId');
 %         size(uvw)
 %         u1 = uvw(obsId==3, 1)*fmax/speed_of_light;
 %         v1 = uvw(obsId==3, 2)*fmax/speed_of_light;
@@ -179,29 +171,29 @@ else
     end
     bmax = max(sqrt(u1.^2 + v1.^2));
 
-    % cellsize = 3600*180/(param_synth.superresolution_factor*2*pi*bmax); % in arcsec
-    u = u1 * pi / (param_synth.superresolution_factor * bmax);
-    v = v1 * pi / (param_synth.superresolution_factor * bmax);
+    % cellsize = 3600*180/(superresolution_factor*2*pi*bmax); % in arcsec
+    u = u1 * pi / (superresolution_factor * bmax);
+    v = v1 * pi / (superresolution_factor * bmax);
     size(u);
     disp('Coverage loaded successfully');
     clear uvw u1 v1;
 end
 
 %% Setup parpool
-cirrus_cluster = util_set_parpool('hs', param_model.ncores_data, 1, param_general.flag_cirrus);
+cirrus_cluster = util_set_parpool('hs', ncores_data, 1, flag_cirrus);
 
 %% Setup measurement operator
 % create the measurement operator operator in parallel (depending on
 % the algorithm used)
 spmd
     local_fc = frequencies(rg_c(labindex, 1):rg_c(labindex, 2));
-    if param_model.flag_DR
+    if flagDR
         % ! define Sigma (weight matrix involved in DR)
         % ! define G as the holographic matrix
     else
         % ! ideally, simplify irt nufft interface to do so
         [A, At, G, W, aW] = util_gen_measurement_operator(u, v, ...
-        param_precond, param_blocking, local_fc, fmax, Nx, Ny, param_nufft.Kx, param_nufft.Ky, param_nufft.ox, param_nufft.oy, param_nufft.kernel);
+        param_precond, param_blocking, local_fc, fmax, Nx, Ny, param_nufft.Kx, param_nufft.Ky, param_nufft.ox, param_nufft.oy, kernel);
         Sigma = [];
     end
 end
@@ -218,9 +210,8 @@ param_l2_ball.sigma_ball = 2;
 % https://fr.mathworks.com/help/matlab/math/creating-and-controlling-a-random-number-stream.html?searchHighlight=random%20number%20streams&s_tid=srchtitle#brvku_2
 % may not be strictly equivalent to the data initially obtained for the
 % mnras paper
-[rng_stream{1:param_model.ncores_data}] = ...
-    RandStream.create('threefry4x64_20', 'Seed', seed, 'NumStreams', ...
-    param_model.ncores_data);
+[rng_stream{1:ncores_data}] = RandStream.create('threefry4x64_20', ...
+    'Seed', seed, 'NumStreams', ncores_data);
 
 spmd
     [y0, y, Ml, ~, sigma_noise, ~] = util_gen_measurements_snr( ...
@@ -241,7 +232,7 @@ datafile.sigma_noise = zeros(nchans, 1);
 % ! need to convert from local to global channel index (i.e., index 
 % ! into the full spectral dimension by using 
 % ! subcube_channels(rg_c(k, 1))
-for k = 1:param_model.ncores_data
+for k = 1:ncores_data
     datafile.y0(rg_c(k, 1):rg_c(k, 2), 1) = y0{k};
     datafile.y(rg_c(k, 1):rg_c(k, 2), 1) = y{k};
     datafile.epsilons(rg_c(k, 1):rg_c(k, 2), 1) = epsilons{k};
--- experiments/script_example/read_json_configuration.m
+++ experiments/script_example/read_json_configuration.m
@@ -1,4 +1,4 @@
-function [speed_of_light, param_paths, param_model, param_solver, ...
+function [speed_of_light, param_general, param_model, param_solver, ...
     param_nufft, param_blocking, param_precond, param_synth, param_nnls] = ...
     read_json_configuration(json_filename, ind)
 
@@ -15,7 +15,7 @@ config = jsondecode(str);
 speed_of_light = 299792458;
 
 %% Paths (to dataset and uv-coverage)
-param_paths = config{1, 1}.general;
+param_general = config{1, 1}.general;
 
 %% (Optional) Parameters for synthetic data generation (only useful for 
 % synthetic data scirpts)
@@ -24,16 +24,17 @@ param_synth = config{1, 1}.synth;
 %% Solver structures
 % * Reweighting
 % https://fr.mathworks.com/matlabcentral/answers/273955-how-do-i-rename-fields-of-a-structure-array
-param_reweighting = cell2struct(struct2cell(config{3, 1}.reweighting), strcat(fieldnames(config{2, 1}.reweighting), 'reweighting_'));
+param_reweighting = cell2struct(struct2cell(config{2, 1}.reweighting), strcat('reweighting_', fieldnames(config{2, 1}.reweighting), 'reweighting_'));
 
 % * PDFB
-param_pdfb = cell2struct(struct2cell(config{2, 1}.pdfb), strcat(fieldnames(config{2, 1}.pdfb), 'pdfb_'));
+param_pdfb = cell2struct(struct2cell(config{2, 1}.pdfb), strcat('pdfb_', fieldnames(config{2, 1}.pdfb)));
 
 % * Projection onto the ellipsoid
-param_proj = cell2struct(struct2cell(config{2, 1}.proj), strcat(fieldnames(config{2, 1}.proj), 'elipse_proj_'));
+param_proj = cell2struct(struct2cell(config{2, 1}.proj), strcat('elipse_proj_', fieldnames(config{2, 1}.proj)));
 
 % combining the 2 structures (reweighting and pdfb)
-param_solver = [param_reweighting, param_pdfb, param_proj];
+param_solver = cell2struct([struct2cell(param_reweighting); struct2cell(param_pdfb);struct2cell(param_proj)], ...
+    [fieldnames(param_reweighting);fieldnames(param_pdfb);fieldnames(param_proj)]);
 param_solver.cube_id = ind;
 
 %% Model structures
@@ -52,10 +53,10 @@ param_blocking.fpartition = [icdf('norm', 0.25, 0, pi / 4), 0, icdf('norm', 0.75
 % set weighting type
 param_precond = config{3, 1}.preconditioning;
 % number of pixels in the image
-param_precond.N = Nx * Ny;
+param_precond.N = param_model.Nx * param_model.Ny;
 % number of Fourier points (oversampled plane)
-param_precond.Nox = param_nufft.ox * Nx;
-param_precond.Noy = param_nufft.oy * Ny;
+param_precond.Nox = param_nufft.ox * param_model.Nx;
+param_precond.Noy = param_nufft.oy * param_model.Ny;
 
 % * NNLS
 param_nnls = config{3, 1}.nnls;
