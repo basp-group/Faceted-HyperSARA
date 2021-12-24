0e3a1bf3c48446c78da1ef61bdceea3832215faa
Arwa Dabbech <dabbecharoua@gmail.com>
2021-12-24 13:32:31 +0000

    op. updated for the dr case

--- lib/utils/util_operator_norm.m
+++ lib/utils/util_operator_norm.m
@@ -1,29 +1,29 @@
-function [Anorm, squared_operator_norm, rel_var, squared_precond_operator_norm, rel_var_precond] = util_operator_norm(G, W, A, At, aW, Ny, Nx, tol, max_iter,flag_dr)
-if nargin<10
-    flag_dr=0;
+function [Anorm, squared_operator_norm, rel_var, squared_precond_operator_norm, rel_var_precond] = util_operator_norm(G, W, A, At, aW, Ny, Nx, tol, max_iter,flag_dr,Sigma)
+if nargin<10 
+    flag_dr=0; Sigma =[];
 end
-    nchans = numel(G);
-    squared_precond_operator_norm = zeros(nchans, 1);
-    rel_var_precond = zeros(nchans, 1);
+nchans = numel(G);
+squared_precond_operator_norm = zeros(nchans, 1);
+rel_var_precond = zeros(nchans, 1);
 
-    for l = 1:nchans
-        F = afclean(@(x) HS_forward_operator_precond_G(x, G(l), W(l), A, aW(l),flag_dr));
-        Ft = afclean(@(y) HS_adjoint_operator_precond_G(y, G(l), W(l), At, aW(l), Ny, Nx,flag_dr));
-        [squared_precond_operator_norm(l), rel_var_precond(l)] = op_norm(F, Ft, [Ny, Nx], tol, max_iter, 0);
-    end
+for l = 1:nchans
+    F = afclean(@(x) HS_forward_operator_precond_G(x, G(l), W(l), A, aW(l),flag_dr,Sigma));
+    Ft = afclean(@(y) HS_adjoint_operator_precond_G(y, G(l), W(l), At, aW(l), Ny, Nx,flag_dr,Sigma));
+    [squared_precond_operator_norm(l), rel_var_precond(l)] = op_norm(F, Ft, [Ny, Nx], tol, max_iter, 0);
+end
 
-    % ! operator is block diagonal: it norm is thus given by the max singular value
-    % ! make sure the pdfb convergence criterion is strictly satisfied
-    % ! by taking a smaller step-size (take precision of the estimation
-    % ! of the operator norm into account)
-    Anorm = max(squared_precond_operator_norm .* (1 + rel_var_precond));
+% ! operator is block diagonal: it norm is thus given by the max singular value
+% ! make sure the pdfb convergence criterion is strictly satisfied
+% ! by taking a smaller step-size (take precision of the estimation
+% ! of the operator norm into account)
+Anorm = max(squared_precond_operator_norm .* (1 + rel_var_precond));
 
-    squared_operator_norm = zeros(nchans, 1);
-    rel_var = zeros(nchans, 1);
-    for l = 1:nchans
-        F = afclean(@(x) HS_forward_operator_G(x, G(l), W(l), A,flag_dr));
-        Ft = afclean(@(y) HS_adjoint_operator_G(y, G(l), W(l), At, Ny, Nx,flag_dr));
-        [squared_operator_norm(l), ~] = op_norm(F, Ft, [Ny, Nx], tol, max_iter, 0);
-    end
+squared_operator_norm = zeros(nchans, 1);
+rel_var = zeros(nchans, 1);
+for l = 1:nchans
+    F = afclean(@(x) HS_forward_operator_G(x, G(l), W(l), A,flag_dr,Sigma));
+    Ft = afclean(@(y) HS_adjoint_operator_G(y, G(l), W(l), At, Ny, Nx,flag_dr,Sigma));
+    [squared_operator_norm(l), ~] = op_norm(F, Ft, [Ny, Nx], tol, max_iter, 0);
+end
 
 end
