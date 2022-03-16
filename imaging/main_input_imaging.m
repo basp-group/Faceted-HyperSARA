d8f67f9b57e5fdf6dbf3aaac1a7c894eebc559db
Arwa Dabbech <dabbecharoua@gmail.com>
2022-03-16 20:09:30 +0000

    comments updated

--- imaging/main_input_imaging.m
+++ imaging/main_input_imaging.m
@@ -158,16 +158,15 @@ cd(project_dir);
 
 % src name & datasets
 % TODO: to be adjusted by the user
-srcName = 'CYGA';
-datasetNames = {'CYGA-ConfigA', 'CYGA-ConfigC'}; % allowing for multiple datasets, leave empty if single dataset
+srcName = 'CYGA'; % specified during data extraction
+datasetNames = {'CYGA-ConfigA', 'CYGA-ConfigC'}; % allowing for multiple datasets, leave empty if no nametags of the measurement set is specified during data extraction
 
 % data directory
 % TODO: to be adjusted by the user
 data_dir = [main_dir, filesep, 'data', filesep, srcName, filesep];
 fprintf('\nINFO: data are expected to be saved at %s\n', data_dir);
 % preproc dir.
-% preproc_calib_dir = [data_dir, 'pre_processing_die/'];
-preproc_calib_dir = [data_dir, 'pre_processing_dde/']; % set to [] if not used 
+preproc_calib_dir = [data_dir, 'pre_processing_dde/']; 
 % name of json parameter file
 json_filename = "default_parameters.json";
 
@@ -198,10 +197,10 @@ dataFilename = @(idSet, ch) strcat(data_dir, filesep, datasetNames{idSet}, files
 % > nChannelsPerImage = 16;
 
 % TODO: to be adjusted by the user
-idChannels2Image = 289; % ids of the 'physical' channels to be imaged, e.g., [1:2] for channel range from 1 to 2
-nChannelsPerImage = 1; % number of consecutive channels to be combined into each effective channel
+idChannels2Image = [1:256]; % ids of the 'physical' channels to be imaged, e.g., [1:256] for channel range from 1 to 256
+nChannelsPerImage = 16; % number of consecutive channels to be combined into each effective channel
 
-nEffChans2Image = floor(numel(idChannels2Image) / nChannelsPerImage); % effective channels: number of images in the estimate model cube
+nEffChans2Image = floor(numel(idChannels2Image) / nChannelsPerImage); % effective channels: number of output images in the estimate model cube
 % It must be equal to 1 for 'sara' and >1 for 'fhs'
 
 %re-arranging channels indices based on idChannels2Image, nChannelsPerImage and nEffChans2Image
@@ -219,7 +218,7 @@ subcubeInd = 0; % id of subcube if spectral interleaving is active, 0 if inactiv
 
 % measurement op. params
 % TODO: to be adjusted by the user
-param_global.measop_flag_visibility_gridding = 0; % 1 if feature used, 0 otherwise
+param_global.measop_flag_visibility_gridding = 0; % 1 if active, 0 otherwise: activating visibility gridding to reduce data dimensionality
 
 % image details, dims &  cellsize
 % TODO: to be adjusted by the user
@@ -232,15 +231,14 @@ param_global.algo_flag_computeOperatorNorm = true;
 % faceting params: note that if interleaving is active, one subcube is
 % imaged at a time: Qc=1 by default.
 % TODO: to be adjusted by the user
-param_global.facet_Qx = 1; % number of spatial facets along axis x (only used in Faceted HyperSARA)
-param_global.facet_Qy = 1; % number of spatial facets along axis y (only used in Faceted HyperSARA)
-param_global.facet_overlap_fraction = [0.5, 0.5];
+param_global.facet_Qx = 5; % number of spatial facets along axis x (only used in Faceted HyperSARA)
+param_global.facet_Qy = 3; % number of spatial facets along axis y (only used in Faceted HyperSARA)
+param_global.facet_overlap_fraction = [0.1, 0.1];
 
 % reg params
 % TODO: to be adjusted by the user
 param_global.reg_gam = 0.33; % additional scaling factor for sparsity 
-% regularization (using heuristics described in Thouvenin2021). Set 
-% parameter to 1 by default.
+% regularization (using heuristics described in Thouvenin2021). Set parameter to 1 by default.
 param_global.reg_gam_bar = 0.33; % additional scaling factor for low-rankness regularization (using heuristics described in Thouvenin2021). Only active 
 % for HyperSARA and Faceted HyperSARA. Set parameter to 1 by default.
 param_global.reg_flag_reweighting = true;
@@ -248,20 +246,22 @@ param_global.reg_nReweights = 5;
 
 % algo & parallelisation params
 % TODO: to be adjusted by the user
-param_global.algo_version = 'sara'; % 'fhs' (Faceted HyperSARA), 'hs' (HyperSARA) or 'sara' (SARA approach)
+param_global.algo_version = 'fhs'; % 'fhs' (Faceted HyperSARA), 'hs' (HyperSARA) or 'sara' (SARA approach)
 
 % filenames and input
 % TODO: to be adjusted by the user (set path to [] whenever it is not used)
 param_global.main_dir = main_dir;
- param_global.preproc_filename_dde = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'ddes', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_ddes.mat');
+param_global.preproc_filename_die = []; % ! must be set to [] or commented if not used 
+param_global.preproc_filename_dde = []; % ! must be set to [] or commented if not used 
+% param_global.preproc_filename_dde = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'ddes', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_ddes.mat');
 % param_global.preproc_filename_die = @(firstch, lastch) strcat(preproc_calib_dir, filesep,'dies',filesep,'chs', num2str(firstch), '-', num2str(lastch), '_dies.mat');
-% param_global.preproc_filename_die = [];
-%
-param_global.preproc_filename_l2bounds = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'l2bounds', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_l2bounds.mat');
-% param_global.preproc_filename_l2bounds = [];
-%
-param_global.preproc_filename_model = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'model_images', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_model_image.fits');
-% param_global.preproc_filename_model = [];
+
+param_global.preproc_filename_l2bounds = []; % ! must be set to [] or commented if not used 
+%param_global.preproc_filename_l2bounds = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'l2bounds', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_l2bounds.mat');
+
+param_global.preproc_filename_model = [];
+%param_global.preproc_filename_model = @(firstch, lastch) strcat(preproc_calib_dir, filesep, 'model_images', filesep, 'chs', num2str(firstch), '-', num2str(lastch), '_model_image.fits');
+
 
 % hardware
 % TODO: to be adjusted by the user
